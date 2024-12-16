c8d8baab3d31ebe8d407d09b2ba9b4c564076768
yuyeh <herry861212@gmail.com>
2024-12-16 22:01:19 +0800

    新增每日任務

---
+++ 每日任務/day01~10/day01.js
@@ -0,0 +1,25 @@
+// codepen: https://codepen.io/yuyeh1212/pen/emOgdVZ
+
+// 變數:
+// 1. 我的 Discord 名稱
+const myDiscordName = "yuyeh_1212";
+// 2. 餐廳數量
+const numberOfRestaurants = 2;
+// 3. 課程 id
+const courseID = 123456789;
+// 4. 畢業日期
+const graduationDate = "2024/06/01";
+// 5. 練習時間
+const practiceTime = 2;
+
+// 函式:
+// 1. 取得產品資料
+function getProductData() {}
+// 2. 渲染圖表
+function renderChart() {}
+// 3. 篩選購物車資料
+function filterCartData() {}
+// 4. 排序註冊日期
+function sortRegistrationDate() {}
+// 5. 刪除訂單
+function deleteOrder() {}
